0c804f286b461bd39a2004fa12e1e924c9aaaa48
cddawson <cddawson@umich.edu>
2024-02-03 16:43:35 -0500

    routing

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+
+    #return to an html file - asking for user input 
+    #return "Hello, World!"
+
+    
+    return render_template('first.html', title='Home')
+
+@app.route('/output')
+def output():
+    return render_template('output.html', title='Home')
